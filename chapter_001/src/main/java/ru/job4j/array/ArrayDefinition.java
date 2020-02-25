05f2fd1aa42e414f8fe86abd89557e872a5c1bb4
Rexgrid <daervia93@gmail.com>
2020-02-25 20:55:43 +0300

    Заполнение массива [#231673]

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,13 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float [] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Alex";
+        names[1] = "Mathew";
+        names[2] = "Grisha";
+        names[3] = "Josh";
+        for (int i = 0; i <= names.length-1; i++) {
+            System.out.println (names[i]);
+        }
     }
 }
