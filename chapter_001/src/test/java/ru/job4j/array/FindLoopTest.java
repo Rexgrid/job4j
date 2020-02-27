8e84f0e8947ea7b4bf66220d49f3260aed7d7d21
Rexgrid <daervia93@gmail.com>
2020-02-27 22:53:42 +0300

    Поиск индекса в диапазоне [#231662]

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -26,4 +26,25 @@ public class FindLoopTest {
         assertThat(result, is(expect));
     }
 
+    @Test
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFindMinusOne(){
+        int[] input = new int[]{1, 2, 3, 4, 5};
+        int value = 7;
+        int start = 1;
+        int finish = 3;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
 }
