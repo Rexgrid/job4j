a61822d65c12edd22d8749d4d06bb491f36a30ec
Rexgrid <daervia93@gmail.com>
2020-02-27 21:25:22 +0300

    Перевернуть массив [#231676]

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class Turn {
     public static int[] back(int[] array) {
-        for (int tmp = 0; tmp < array.length - tmp; tmp++) {
+        for (int tmp = 0; tmp < array.length /2; tmp++) {
         int temp = array[tmp];
         array[tmp] = array[array.length - tmp - 1];
         array[array.length - tmp - 1] = temp;
