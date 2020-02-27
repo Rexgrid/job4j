54d112fe3d5b3153f9e0ce455a06f6e650551b1b
Rexgrid <daervia93@gmail.com>
2020-02-27 22:41:18 +0300

    Слово заканчивается с [#231661]

---
+++ chapter_001/src/main/java/ru/job4j/array/EndWith.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class EndWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = false;
+       for (int i = 0; i < word.length && i < post.length; i++){
+           if (post[i] == word[i+(word.length-post.length)]){
+               result = true;
+           }
+           else {
+               result = false;
+           }
+       }
+        return result;
+    }
+}
