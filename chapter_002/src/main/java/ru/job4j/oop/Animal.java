4af44dbc3b5971ea11a3081150737972f8df9aed
Rexgrid <daervia93@gmail.com>
2020-03-30 16:50:46 +0300

    Перегрузка конструктора [#231832]

--- chapter_002/src/main/java/ru/job4j/oop/Animal.java
+++ chapter_002/src/main/java/ru/job4j/oop/Animal.java
@@ -1,8 +1,11 @@
 package ru.job4j.oop;
 
 public class Animal {
+
     public Animal() {
-        super();
-        System.out.println("Animal");
     }
+
+    public Animal(String name) {
+    }
+
 }
