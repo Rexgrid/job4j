bf89f75caf210f7c285b58753f40c770dbad5f25
Rexgrid <daervia93@gmail.com>
2020-04-04 18:24:08 +0300

    Удаление моделей из массива[#231841]

---
+++ chapter_002/src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,88 @@
+package ru.job4j.pojo;
+
+public class Shop {
+
+    public static void main(String[] args) {
+
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+
+
+        delete(products, 0);
+        for (int index = 0; index < products.length; index++) {
+            Product s = products[index];
+            if (s != null) {
+                System.out.println(s.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+
+       /* for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            //проверяем, что объект не равен null. тк у нас массив не заполнен целиком.
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Удаляем значение из ячейки с индексом 1");
+        //удаляем значение из ячейки с индексом 1
+        products[1] = null;
+
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            //проверяем, что объект не равен null. тк у нас массив не заполнен целиком.
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Записываем  в ячейку с индексом 1 значение ячейки с индексом 2 и удаляем значение из ячейки с индексом 2");
+        //записываем в ячейку с индексом 1 значение ячейки с индексом 2.
+        products[1] = products[2];
+        //удаляем значение из ячейки с индексом 2.
+        products[2] = null;
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            //проверяем, что объект не равен null. так как  у нас массив не заполнен целиком.
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+
+    }
+
+        */
+    }
+
+
+
+    public static Product[] delete(Product[] products, int index) {
+            for (int in = index; in < products.length; in++) {
+                if (in + 1 < products.length) {
+                    products[in] = products[in + 1];
+                }
+            }
+            products[products.length - 1] = null;
+
+            return products;
+
+        }
+    }
+
+
+
+
