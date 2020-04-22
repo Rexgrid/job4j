60862290d86815aec20efe44d2d43fefe40452e0
Rexgrid <daervia93@gmail.com>
2020-04-22 14:23:28 +0300

    Рефакторинг теста @Before @After [#231786]

--- chapter_002/src/test/java/ru/job4j/stragery/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/stragery/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.stragery;
 
+import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -8,40 +10,22 @@ import static org.junit.Assert.assertThat;
 import  static org.hamcrest.core.Is.is;
 
 public class PaintTest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
     @Test
     public void whenDrawSquare() {
-        Square square = new Square();
-        assertThat(
-                square.draw(),
-                is(
-                        new StringJoiner(System.lineSeparator())
-                                .add("++++")
-                                .add("+     +")
-                                .add("+     +")
-                                .add("++++")
-                                .toString()
-                )
-        );
-    }
-    @Test
-    public void whenDrawTriangle() {
-        Triangle triangle = new Triangle();
-        assertThat(
-                triangle.draw(),
-                is(
-                        new StringJoiner(System.lineSeparator())
-                        .add("+")
-                        .add("+ +")
-                        .add("+++")
-                        .toString()
-                )
-        );
-    }
-    @Test
-    public void whenDrawSquare1() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(
                 new String(out.toByteArray()),
@@ -54,13 +38,9 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        System.setOut(stdout);
     }
     @Test
-    public void whenDrawTriangle1() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
+    public void whenDrawTriangle() {
         new Paint().draw(new Triangle());
         assertThat(
                 new String(out.toByteArray()),
@@ -72,6 +52,5 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        System.setOut(stdout);
     }
 }
