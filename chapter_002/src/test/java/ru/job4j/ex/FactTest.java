53fcb9e5edf5c0937938c9fb7e1e309f62f7e653
Rexgrid <daervia93@gmail.com>
2020-04-28 21:26:04 +0300

    Тестирование исключений с junit [#231808]

---
+++ chapter_002/src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,13 @@
+package ru.job4j.ex;
+
+import ex.Fact;
+import org.junit.Test;
+
+public class FactTest {
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNLess0ThenIAE() {
+    Fact.calc(-1);
+}
+}
