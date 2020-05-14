664a38bb55eb5177c4d17575341cb187d0b07c99
Rexgrid <daervia93@gmail.com>
2020-05-15 01:47:19 +0300

    Телефонный справочник на базе ArrayList [#231754]

---
+++ chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,28 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     * @param key Ключ поиска.
+     * @return Список подощедщих пользователей.
+     */
+
+
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person value : persons) {
+            if (value.toString().contains(key)) {
+               result.add(value);
+            }
+        }
+        return result;
+        }
+    }
