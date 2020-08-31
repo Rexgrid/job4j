709119eb944ad7b4111de882177c26c8e25f6c3a
Rexgrid <daervia93@gmail.com>
2020-08-31 20:57:56 +0300

    Отсортировать департаменты [#231745]

--- chapter_002/src/main/java/ru/job4j/collection/DepDescComp.java
+++ chapter_002/src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,9 +5,9 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        String[] lft = o1.split("\\. ");
-        String[] rgh = o2.split("\\. ");
-        int rsl = lft[0].compareTo(rgh[0]);
+        String[] lft = o1.split("\\ ");
+        String[] rgh = o2.split("\\ ");
+        int rsl = rgh[0].compareTo(lft[0]);
         if (rsl != 0) {
             return rsl;
         } else {
--- chapter_002/src/test/java/ru/job4j/collections/DepartmentsTest.java
+++ chapter_002/src/test/java/ru/job4j/collections/DepartmentsTest.java
@@ -2,6 +2,7 @@ package ru.job4j.collections;
 
 import org.hamcrest.core.Is;
 import org.junit.Test;
+import ru.job4j.collection.DepDescComp;
 import ru.job4j.collection.Departments;
 
 import java.util.Arrays;
@@ -27,5 +28,21 @@ public class DepartmentsTest {
         List<String> expect = Arrays.asList("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenAscSorted() {
+        List<String> input = Arrays.asList("K2/SK1/SSK2", "K2", "K2/SK1");
+        Departments.sortAsc(input);
+        List<String> expected = Arrays.asList("K2", "K2/SK1", "K2/SK1/SSK2");
+        assertThat(input, is(expected));
+    }
+
+    @Test
+    public void whenDescSorted() {
+        List<String> input = Arrays.asList("K2", "K2/SK1", "K2/SK1/SSK2");
+        Departments.sortDesc(input);
+        List<String> expected = Arrays.asList("K2/SK1/SSK2", "K2/SK1", "K2");
+        assertThat(input, is(expected));
     }
 }
