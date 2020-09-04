7f4201e40ce6cb5f8019cd5381d861a6e2337edd
Rexgrid <daervia93@gmail.com>
2020-09-04 20:12:54 +0300

    Stream API [#231619]

---
+++ chapter_002/src/main/java/ru/job4j/lambda/Stream.java
@@ -0,0 +1,15 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Stream {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList(Arrays.asList(-1, -2, -3, 0, 1, 2, 3));
+        List<Integer> nums = numbers.stream().filter(n -> n > 0).collect(Collectors.toList());
+        nums.forEach(System.out::println);
+    }
+}
+
