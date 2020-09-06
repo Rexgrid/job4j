876b39e075b5dbb43f2a231de46cdc23e8952fe6
Rexgrid <daervia93@gmail.com>
2020-09-06 19:53:30 +0300

    Преобразование List в Map. [#231623]

--- chapter_002/src/main/java/ru/job4j/stream/School.java
+++ chapter_002/src/main/java/ru/job4j/stream/School.java
@@ -1,11 +1,18 @@
 package ru.job4j.stream;
 
+import ru.job4j.collection.LexSort;
+
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class School {
     List<Student> collect(List<Student> students, Predicate<Student> predict) {
         return students.stream().filter(predict).collect(Collectors.toList());
+    }
+
+    Map<String, Student> students(List<Student> student) {
+        return  student.stream().distinct().collect(Collectors.toMap(Student::getSurname, e -> e));
     }
 }
