cb41e14f84eac45d8c0630cf8ff6441a7e83f678
Rexgrid <daervia93@gmail.com>
2021-08-26 17:09:05 +0300

    Liquibase. Интеграционные тесты. Proxy

--- chapter_002/src/main/java/ru/job4j/tracker/SqlTracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -12,6 +12,12 @@ public class SqlTracker implements Store {
     private Connection cn;
     private final List<Item> items = new ArrayList<>();
 
+    public SqlTracker(Connection cn) {
+        this.cn = cn;
+    }
+    public SqlTracker() {
+    }
+
     @Override
     public void init() {
         try (InputStream in = SqlTracker.class.getClassLoader().getResourceAsStream("app.properties")) {
@@ -33,16 +39,16 @@ public class SqlTracker implements Store {
     @Override
     public Item add(Item item) {
         try (PreparedStatement statement = cn.prepareStatement(
-                "insert into items(name, created_time) values (?,?)", Statement.RETURN_GENERATED_KEYS)) {
+                "insert into items(name, created) values (?,?)", Statement.RETURN_GENERATED_KEYS)) {
             statement.setString(1, item.getName());
             statement.setTimestamp(2, Timestamp.valueOf(item.getCreated()));
             statement.execute();
             try (ResultSet generatedKey = statement.getGeneratedKeys()) {
-                if(generatedKey.next()) {
+                if (generatedKey.next()) {
                     item.setId(generatedKey.getInt(1));
                 }
             }
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return item;
@@ -52,10 +58,10 @@ public class SqlTracker implements Store {
     public boolean replace(int id, Item item) {
         boolean result = false;
        try (PreparedStatement statement = cn.prepareStatement(
-               "update items set name = ?, created_time = ? where id = ? ")){
-            statement.setString(1,item.getName());
+               "update items set name = ?, created = ? where id = ? ")) {
+            statement.setString(1, item.getName());
             statement.setTimestamp(2, Timestamp.valueOf(item.getCreated()));
-            statement.setInt(3, item.getId());
+            statement.setInt(3, id);
             result = statement.executeUpdate() > 0;
        }
        catch (Exception e) {
@@ -79,7 +85,7 @@ public class SqlTracker implements Store {
     @Override
     public List<Item> findAll() {
         try (PreparedStatement statement = cn.prepareStatement("select * from items")) {
-            try(ResultSet resultSet = statement.executeQuery()) {
+            try (ResultSet resultSet = statement.executeQuery()) {
                 while (resultSet.next()) {
                     items.add(new Item(
                             resultSet.getString("name"),
@@ -132,7 +138,7 @@ public class SqlTracker implements Store {
 
     @Override
     public void close() throws Exception {
-        if(cn != null) {
+        if (cn != null) {
             cn.close();
         }
     }
